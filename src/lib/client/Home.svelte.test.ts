3994425f7e4bceaed3883b5feb5ec1c87746b844
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-03 20:06:28 +0100

    chore: update svelte component and test files

--- src/lib/client/Home.svelte.test.ts
+++ src/lib/client/Home.svelte.test.ts
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/svelte/svelte5";
-import Home from "./home.svelte";
+import Home from "./Home.svelte";
 
 describe("/home.svelte", () => {
   test("should render h1", () => {
