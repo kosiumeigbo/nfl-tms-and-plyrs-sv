5b6ce0d055603e7c7a4e82b924242a905d55edec
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-04 14:33:48 +0100

    chore: create new home.svelte.test.ts file to test out issues

---
+++ src/lib/client/home.svelte.test.ts
@@ -0,0 +1,10 @@
+import Home from "./home.svelte";
+import { render, screen } from "@testing-library/svelte";
+
+describe("Home component", () => {
+  test("it renders", () => {
+    render(Home);
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(heading).toBeInTheDocument();
+  });
+});
