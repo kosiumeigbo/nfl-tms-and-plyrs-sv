353fb13468e0f3da1a72dc4b6e8d7e94553d48ec
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-06 08:10:05 +0100

    chore: return the state after mutation

--- src/lib/client/components/modules/home-page/home-page.svelte.ts
+++ src/lib/client/components/modules/home-page/home-page.svelte.ts
@@ -9,4 +9,5 @@ export const getHeaderTabState = function () {
 export const setHeaderTabState = function (newState: HeaderTabState) {
   headerTabState = newState;
   console.log($state.snapshot(headerTabState));
+  return headerTabState;
 };
