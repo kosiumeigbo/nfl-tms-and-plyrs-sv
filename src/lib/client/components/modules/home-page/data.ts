9c31a19896fa1a57dbbc8b3d4b2f795c472275d9
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-09 23:49:55 +0100

    chore: create data object for home page teams table

--- src/lib/client/components/modules/home-page/data.ts
+++ src/lib/client/components/modules/home-page/data.ts
@@ -1,1 +1,14 @@
-const allTeams = {};
+export const allTeams = {
+  NFC: {
+    North: ["CHI", "DET", "GB", "MIN"],
+    South: ["ATL", "CAR", "NO", "TB"],
+    West: ["ARI", "LAR", "SEA", "SF"],
+    East: ["DAL", "NYG", "PHI", "WAS"],
+  },
+  AFC: {
+    North: ["BAL", "CIN", "CLE", "PIT"],
+    South: ["HOU", "IND", "JAX", "TEN"],
+    West: ["DEN", "KC", "LAC", "LV"],
+    East: ["BUF", "MIA", "NE", "NYJ"],
+  },
+} as const;
