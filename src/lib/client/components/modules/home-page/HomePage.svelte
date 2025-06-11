e346789e2d4e9c18028d31d25977fd2060389f76
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-11 23:43:32 +0100

    feat: create PlayersSection component

--- src/lib/client/components/modules/home-page/HomePage.svelte
+++ src/lib/client/components/modules/home-page/HomePage.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import Header from "./Header.svelte";
   import TeamsSection from "./TeamsSection.svelte";
+  import PlayersSection from "./PlayersSection.svelte";
   import { getHeaderTabState } from "./home-page.svelte";
 </script>
 
 <Header />
-
 <TeamsSection isNotDisplayed={getHeaderTabState() === null || getHeaderTabState() !== "teams"} />
-<div class={{ "hidden": getHeaderTabState() === null || getHeaderTabState() !== "players" }}>Players</div>
+<PlayersSection isNotDisplayed={getHeaderTabState() === null || getHeaderTabState() !== "players"} />
---
+++ src/lib/client/components/modules/home-page/PlayersSection.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { effectFunction } from "./home-page.svelte";
+
+  let section: HTMLElement | undefined = undefined;
+
+  $effect(() => effectFunction({ node: section, sectionStatus: "players" }));
+
+  let { isNotDisplayed }: { isNotDisplayed: boolean } = $props();
+</script>
+
+<section bind:this={section} class={{ "hidden": isNotDisplayed, "block": !isNotDisplayed }}>
+  <hr />
+  Players
+</section>
