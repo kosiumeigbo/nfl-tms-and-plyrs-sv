dedc9d0055fdb4e932bff4f9e6aa2ab739bf0ae4
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-11 02:14:48 +0100

    refactor: state variables, functions and effectFunction

--- src/lib/client/components/modules/home-page/home-page.svelte.ts
+++ src/lib/client/components/modules/home-page/home-page.svelte.ts
@@ -1,4 +1,4 @@
-type HeaderTabStatus = { status: "teams"; buttonPressed: number } | { status: "players"; buttonPressed: number };
+type HeaderTabStatus = "teams" | "players";
 type HeaderTabState = null | HeaderTabStatus;
 
 let headerTabState: HeaderTabState = $state(null);
@@ -7,18 +7,8 @@ export const getHeaderTabState = function () {
   return headerTabState;
 };
 
-export const setHeaderTabState = function (newStatus: HeaderTabStatus["status"]) {
-  const currentHeaderTabState = getHeaderTabState();
-  if (currentHeaderTabState === null) {
-    headerTabState = { status: newStatus, buttonPressed: 1 };
-  } else {
-    if (currentHeaderTabState.status === newStatus) {
-      headerTabState = { status: newStatus, buttonPressed: currentHeaderTabState.buttonPressed + 1 };
-    } else {
-      headerTabState = { status: newStatus, buttonPressed: 1 };
-    }
-  }
-
+export const setHeaderTabState = function (newStatus: HeaderTabStatus) {
+  headerTabState = newStatus;
   console.log($state.snapshot(headerTabState));
   return headerTabState;
 };
@@ -28,23 +18,13 @@ export const effectFunction = function ({
   sectionStatus,
 }: {
   node: HTMLElement | undefined;
-  sectionStatus: HeaderTabStatus["status"];
+  sectionStatus: HeaderTabStatus;
 }) {
   const currentHeaderTabState = getHeaderTabState();
   if (!node) return; // not yet mounted
   if (currentHeaderTabState === null) return;
 
-  let timeoutId: NodeJS.Timeout;
-
-  if (currentHeaderTabState.status === sectionStatus) {
-    if (currentHeaderTabState.buttonPressed === 1) {
-      timeoutId = setTimeout(() => {
-        node?.scrollIntoView({ behavior: "smooth" });
-      }, 10);
-    } else {
-      node?.scrollIntoView({ behavior: "smooth" });
-    }
+  if (currentHeaderTabState === sectionStatus) {
+    node?.scrollIntoView({ behavior: "smooth" });
   }
-
-  return () => clearTimeout(timeoutId);
 };
