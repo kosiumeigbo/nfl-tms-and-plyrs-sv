4e6415a1327b903206dc3e86ed9a4fd1184ac615
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-11 01:22:41 +0100

    refactor: edit setHeaderTabState function

--- src/lib/client/components/modules/home-page/home-page.svelte.ts
+++ src/lib/client/components/modules/home-page/home-page.svelte.ts
@@ -1,4 +1,5 @@
-type HeaderTabState = null | "teams" | "players";
+type HeaderTabStatus = { status: "teams"; buttonPressed: number } | { status: "players"; buttonPressed: number };
+type HeaderTabState = null | HeaderTabStatus;
 
 let headerTabState: HeaderTabState = $state(null);
 
@@ -6,8 +7,18 @@ export const getHeaderTabState = function () {
   return headerTabState;
 };
 
-export const setHeaderTabState = function (newState: HeaderTabState) {
-  headerTabState = newState;
+export const setHeaderTabState = function (newStatus: HeaderTabStatus["status"]) {
+  const currentHeaderTabState = getHeaderTabState();
+  if (currentHeaderTabState === null) {
+    headerTabState = { status: newStatus, buttonPressed: 1 };
+  } else {
+    if (currentHeaderTabState.status === newStatus) {
+      headerTabState = { status: newStatus, buttonPressed: currentHeaderTabState.buttonPressed + 1 };
+    } else {
+      headerTabState = { status: newStatus, buttonPressed: 1 };
+    }
+  }
+
   console.log($state.snapshot(headerTabState));
   return headerTabState;
 };
