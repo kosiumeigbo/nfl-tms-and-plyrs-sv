ea84adf5e3dc0fa15506983559f294c1fcbf1427
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-11 01:37:58 +0100

    chore: create re-useable effectFunction

--- src/lib/client/components/modules/home-page/home-page.svelte.ts
+++ src/lib/client/components/modules/home-page/home-page.svelte.ts
@@ -22,3 +22,29 @@ export const setHeaderTabState = function (newStatus: HeaderTabStatus["status"])
   console.log($state.snapshot(headerTabState));
   return headerTabState;
 };
+
+export const effectFunction = function ({
+  node,
+  sectionStatus,
+}: {
+  node: HTMLElement | undefined;
+  sectionStatus: HeaderTabStatus["status"];
+}) {
+  const currentHeaderTabState = getHeaderTabState();
+  if (!node) return; // not yet mounted
+  if (currentHeaderTabState === null) return;
+
+  let timeoutId: NodeJS.Timeout;
+
+  if (currentHeaderTabState.status === sectionStatus) {
+    if (currentHeaderTabState.buttonPressed === 1) {
+      timeoutId = setTimeout(() => {
+        node?.scrollIntoView({ behavior: "smooth" });
+      }, 10);
+    } else {
+      node?.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  return () => clearTimeout(timeoutId);
+};
