a1c3d0aab0921e9022452c260f4f5ed476d3e850
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-11 01:57:08 +0100

    chore: update HomePage component

--- src/lib/client/components/modules/home-page/HomePage.svelte
+++ src/lib/client/components/modules/home-page/HomePage.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
   import Header from "./Header.svelte";
+  import TeamsSection from "./TeamsSection.svelte";
+  import { getHeaderTabState } from "./home-page.svelte";
 </script>
 
 <Header />
+
+{#if getHeaderTabState()?.status === "teams"}
+  <TeamsSection />
+{:else if getHeaderTabState()?.status === "players"}
+  <div>Players</div>
+{/if}
