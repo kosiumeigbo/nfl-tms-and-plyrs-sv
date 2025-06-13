9ef77982d5a987147c28459b30a6614c946269e3
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-13 08:00:37 +0100

    style: add custom container wrapper

--- src/lib/client/components/modules/home-page/TeamsSection.svelte
+++ src/lib/client/components/modules/home-page/TeamsSection.svelte
@@ -19,7 +19,7 @@
     <div class="grid grid-cols-5">
       <div class="text-2xl">{division}</div>
       {#each getDivisionTeams(conference, division, teamsArray) as team, i (i)}
-        <div><img src={`/logos/${conference}/${division}/${team.Key}.svg`} alt={`Logo for ${team.FullName}`} /></div>
+        <div><img src={team.WikipediaWordMarkUrl} alt={`Logo for ${team.FullName}`} /></div>
         <div><img src={team.WikipediaLogoUrl} alt={`Logo for ${team.FullName}`} /></div>
       {/each}
     </div>
@@ -27,7 +27,9 @@
 {/snippet}
 
 <section bind:this={section} class={{ "hidden": isNotDisplayed, "block": !isNotDisplayed }}>
-  {#each conferences as conference, i (i)}
-    {@render conferenceTable({ conference })}
-  {/each}
+  <div class="custom-container">
+    {#each conferences as conference, i (i)}
+      {@render conferenceTable({ conference })}
+    {/each}
+  </div>
 </section>
