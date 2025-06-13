94d3187588aafb5746b8a61c622971217a4fa7e6
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-13 22:24:46 +0100

    chore: create reuseable data array for home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import HomePage from "@client/components/modules/home-page/HomePage.svelte";
-  import type { PageProps } from "./$types";
+  //   import type { PageProps } from "./$types";
+  import { allTeamsArrayData } from "$lib/client/components/modules/home-page/data";
 
-  let { data }: PageProps = $props();
+  //   let { data }: PageProps = $props();
 </script>
 
-<HomePage teamsArray={data.allTeams} />
+<HomePage teamsArray={allTeamsArrayData} />
