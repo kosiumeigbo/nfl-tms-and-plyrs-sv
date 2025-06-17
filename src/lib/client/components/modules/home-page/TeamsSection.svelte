12f55c926a209d841e0086ec47a491efbb153a67
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-17 22:35:00 +0100

    style: finalise TeamsSection component

--- src/lib/client/components/modules/home-page/TeamsSection.svelte
+++ src/lib/client/components/modules/home-page/TeamsSection.svelte
@@ -22,10 +22,12 @@
   <div class="mb-28">
     {#each divisions as division, i (i)}
       <div
-        class="grid grid-cols-1 grid-rows-[7rem_14rem] lg:grid-cols-[8rem_1fr] lg:grid-rows-[7rem] xl:grid-cols-[15rem_1fr] xl:grid-rows-[10rem]"
+        class="xs:grid-rows-[7rem_14rem] grid grid-cols-1 grid-rows-[7rem_21rem] lg:grid-cols-[8rem_1fr] lg:grid-rows-[7rem] xl:grid-cols-[15rem_1fr] xl:grid-rows-[10rem]"
       >
         <div class="font-display my-auto text-center text-4xl uppercase lg:text-center xl:text-6xl">{division}</div>
-        <div class="grid h-full w-full grid-cols-4 grid-rows-1">
+        <div
+          class="xs:grid-cols-2 xs:grid-rows-2 grid h-full w-full grid-rows-4 gap-y-10 lg:grid-cols-4 lg:grid-rows-1"
+        >
           {#each getDivisionTeams(conference, division, teamsArray) as team, i (i)}
             <div class="flex items-center justify-center">
               <a href={`/team/${team.Key}`} class="flex h-full flex-col items-center justify-center">
