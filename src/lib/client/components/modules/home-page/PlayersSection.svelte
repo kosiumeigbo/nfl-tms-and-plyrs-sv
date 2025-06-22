9d3643cf37a8fe852abcfc1517a5e8b016890dd8
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-22 22:24:33 +0100

    chore: add player search input value tracking

--- src/lib/client/components/modules/home-page/PlayersSection.svelte
+++ src/lib/client/components/modules/home-page/PlayersSection.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
   import { effectFunction } from "./home-page.svelte";
+  import { getInitialPlayerSearchInputValue } from "$lib/context";
 
   let section: HTMLElement | undefined = undefined;
 
   $effect(() => effectFunction({ node: section, sectionStatus: "players" }));
 
   let { isNotDisplayed }: { isNotDisplayed: boolean } = $props();
+
+  let searchInputValue = $state(getInitialPlayerSearchInputValue());
 </script>
 
 <section bind:this={section} class={{ "hidden": isNotDisplayed, "block": !isNotDisplayed, "font-georgia py-10": true }}>
   <div class="custom-container">
     <div class="flex justify-center">
-      <form action="">
+      <form>
         <label class="mb-5 flex flex-col">
           <span class="xs:text-xl mb-1 text-lg md:text-3xl">Enter part or full player name (First or Last):</span>
           <div class="w-full max-w-96">
-            <input type="text" class="w-full border-2 p-2 text-sm outline-none md:text-lg" name="search" />
+            <input
+              type="text"
+              class="w-full border-2 p-2 text-sm outline-none md:text-lg"
+              name="search"
+              bind:value={searchInputValue}
+            />
           </div>
         </label>
         <button
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import HomePage from "@client/components/modules/home-page/HomePage.svelte";
   import type { PageProps } from "./$types";
-  import { setAllTeamsArrayContext } from "$lib/context";
+  import { setAllTeamsArrayContext, setInitialPlayerSearchInputValue } from "$lib/context";
 
   let { data }: PageProps = $props();
   setAllTeamsArrayContext(data.allTeams);
+  setInitialPlayerSearchInputValue(data.initialSearchInput);
 </script>
 
 <HomePage />
