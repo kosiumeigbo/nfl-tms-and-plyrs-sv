1600ac1aeec29394b749b8d28ea92b1353fbdc47
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-22 21:07:36 +0100

    style: player search form UI on home page

--- src/lib/client/components/modules/home-page/PlayersSection.svelte
+++ src/lib/client/components/modules/home-page/PlayersSection.svelte
@@ -8,7 +8,23 @@
   let { isNotDisplayed }: { isNotDisplayed: boolean } = $props();
 </script>
 
-<section bind:this={section} class={{ "hidden": isNotDisplayed, "block": !isNotDisplayed }}>
-  <hr />
-  Players
+<section bind:this={section} class={{ "hidden": isNotDisplayed, "block": !isNotDisplayed, "font-georgia py-10": true }}>
+  <div class="custom-container">
+    <div class="flex justify-center">
+      <form action="">
+        <label class="mb-5 flex flex-col">
+          <span class="xs:text-xl mb-1 text-lg md:text-3xl">Enter part or full player name (First or Last):</span>
+          <div class="w-full max-w-96">
+            <input type="text" class="w-full border-2 p-2 text-sm outline-none md:text-lg" name="search" />
+          </div>
+        </label>
+        <button
+          type="submit"
+          class="hover:from-nfc hover:to-afc cursor-pointer rounded-lg border-2 border-black bg-white px-2 py-0.5 text-base uppercase hover:bg-linear-to-r hover:text-white"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  </div>
 </section>
