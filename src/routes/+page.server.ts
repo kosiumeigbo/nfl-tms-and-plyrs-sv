4f167d309ad18ca07bc57bef57abe9e1816aab6e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-06-23 23:10:04 +0100

    chore: edit server load file

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,17 @@
 import type { PageServerLoad } from "./$types";
 import { getAllTeamsFromSportsdataIOAPI } from "$lib/server";
+import type { Actions } from "./$types";
 
-export const load: PageServerLoad = async function ({ setHeaders }) {
+export const load: PageServerLoad = async function ({ setHeaders, url }) {
+  const initialSearchInput = url.searchParams.get("search") ?? "";
+  console.log(url.searchParams);
   const allTeams = await getAllTeamsFromSportsdataIOAPI();
   setHeaders({ "cache-control": "private, max-age=604800" });
-  return { allTeams };
+  return { allTeams, initialSearchInput };
 };
+
+/* export const actions = {
+  default: () => {
+    return { success: true };
+  },
+} satisfies Actions; */
