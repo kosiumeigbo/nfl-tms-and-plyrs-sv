b81e11a2214b0041b4cfa88f11f48fa7bdacafe9
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-04 08:27:32 +0100

    feat: create function to get all available players from SportsDataIO

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,4 +1,4 @@
-import type { Team } from "$lib/types";
+import type { Team, Player } from "$lib/types";
 import { SPORTSDATA_API_KEY } from "$env/static/private";
 
 export const getAllTeamsFromSportsdataIOAPI = async function () {
@@ -6,4 +6,11 @@ export const getAllTeamsFromSportsdataIOAPI = async function () {
   const res = await fetch(allTeamsAPIUrl);
   const allTeams = (await res.json()) as Team[];
   return allTeams;
+};
+
+export const getAllAvailablePlayersFromSportsdataIOAPI = async function () {
+  const allPlayersAPIURL = `https://api.sportsdata.io/v3/nfl/scores/json/PlayersByAvailable?key=${SPORTSDATA_API_KEY}`;
+  const res = await fetch(allPlayersAPIURL);
+  const allPlayers = (await res.json()) as Player[];
+  return allPlayers;
 };
