2398c27133c48b943852bb5dde334f633a2ea79c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-06 23:49:01 +0100

    refactor: +page.server.ts for individual team page

--- src/routes/team/[key]/+page.server.ts
+++ src/routes/team/[key]/+page.server.ts
@@ -1,9 +1,6 @@
-import { getAllTeamsFromSportsdataIOAPI } from "$lib/server";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async function ({ params, setHeaders }) {
-  const allTeamsArray = await getAllTeamsFromSportsdataIOAPI();
-  const teamObj = allTeamsArray.find((tm) => tm.Key === params.key);
-  setHeaders({ "cache-control": "private, max-age=604800" });
-  return { teamObj };
+export const load: PageServerLoad = function ({ params }) {
+  const { key } = params;
+  return { teamKey: key };
 };
