14d751540746a31c78889241dad36ab4dce396a0
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-06 23:52:10 +0100

    chore: add title tag for the home page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
+export const MAIN_TITLE = "NFL Teams and Players";
 export const ALL_TEAMS_ARRAY_CONTEXT_KEY = "allTeamsArray";
 export const ALL_PLAYERS_ARRAY_CONTEXT_KEY = "allPlayersArray";
 export const INITIAL_PLAYER_SEARCH_INPUT_CONTEXT_KEY = "initialPlayerSearchInput";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import HomePage from "@client/components/modules/home-page/HomePage.svelte";
+  import { MAIN_TITLE } from "$lib/constants";
 </script>
+
+<svelte:head>
+  <title>{MAIN_TITLE}</title>
+</svelte:head>
 
 <HomePage />
