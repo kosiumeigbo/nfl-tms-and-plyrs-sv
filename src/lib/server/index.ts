665259a787c14a0c0e2056ecd046abada423d9f7
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-06 12:34:34 +0100

    chore: add error handling for SportsDataIO API response

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -4,6 +4,9 @@ import { SPORTSDATA_API_KEY } from "$env/static/private";
 export const getAllTeamsFromSportsdataIOAPI = async function () {
   const allTeamsAPIUrl = `https://api.sportsdata.io/v3/nfl/scores/json/Teams?key=${SPORTSDATA_API_KEY}`;
   const res = await fetch(allTeamsAPIUrl);
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
   const allTeams = (await res.json()) as Team[];
   return allTeams;
 };
@@ -11,6 +14,9 @@ export const getAllTeamsFromSportsdataIOAPI = async function () {
 export const getAllAvailablePlayersFromSportsdataIOAPI = async function () {
   const allPlayersAPIURL = `https://api.sportsdata.io/v3/nfl/scores/json/PlayersByAvailable?key=${SPORTSDATA_API_KEY}`;
   const res = await fetch(allPlayersAPIURL);
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
   const allPlayers = (await res.json()) as Player[];
   return allPlayers;
 };
