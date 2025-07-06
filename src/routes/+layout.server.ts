c11040e32cf5f97384fdf3831fb9d862d959b73e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-06 12:47:28 +0100

    chore: add +layout.server.ts to fetch all players and teams

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,15 @@
+import type { LayoutServerLoad } from "./$types";
+import { getAllTeamsFromSportsdataIOAPI, getAllAvailablePlayersFromSportsdataIOAPI } from "$lib/server";
+import type { Team, Player } from "$lib/types";
+
+export const load: LayoutServerLoad = async function ({ setHeaders, url }) {
+  console.log(url.searchParams);
+  try {
+    const promises = Promise.all([getAllTeamsFromSportsdataIOAPI(), getAllAvailablePlayersFromSportsdataIOAPI()]);
+    const [allTeams, allPlayers] = await promises;
+    setHeaders({ "cache-control": "private, max-age=604800" });
+    return { allTeams, allPlayers };
+  } catch (e) {
+    return { allTeams: [], allPlayers: [] } as { allTeams: Team[]; allPlayers: Player[] };
+  }
+};
