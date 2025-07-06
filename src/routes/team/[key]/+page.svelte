595d2ecae6ccf7b810e2fffce906f75c018d9231
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-07 00:06:11 +0100

    chore: update individual team +page.svelte

--- src/routes/team/[key]/+page.svelte
+++ src/routes/team/[key]/+page.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
+  import { getAllTeamsArrayContext } from "$lib/context";
+  import TeamPage from "@client/components/modules/team-page/TeamPage.svelte";
+  import { MAIN_TITLE } from "$lib/constants";
+
   let { data }: PageProps = $props();
+  const teamObj = getAllTeamsArrayContext().find((team) => team.Key === data.teamKey);
+  console.log(teamObj);
+
+  const getPageTitle = function () {
+    if (!teamObj) {
+      return `${MAIN_TITLE} | Team Page`;
+    }
+    return `${MAIN_TITLE} | ${teamObj.Key} | ${teamObj.FullName}`;
+  };
 </script>
 
-<div>{data.teamObj?.City}</div>
+<svelte:head>
+  <title>{getPageTitle()}</title>
+</svelte:head>
+
+{#if !teamObj}
+  <p>Something is wrong</p>
+{:else}
+  <TeamPage {teamObj} />
+{/if}
