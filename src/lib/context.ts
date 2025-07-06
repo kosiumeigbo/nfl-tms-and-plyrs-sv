1e7cc21dac3317fb2adc65ba9bf4e0f30a3ed075
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-06 12:51:04 +0100

    chore: set contexts for all players and teams in root +layout.svelte

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import { getContext, setContext } from "svelte";
-import type { Team } from "./types";
-import { ALL_TEAMS_ARRAY_CONTEXT_KEY } from "./constants";
+import type { Team, Player } from "./types";
+import { ALL_TEAMS_ARRAY_CONTEXT_KEY, ALL_PLAYERS_ARRAY_CONTEXT_KEY } from "./constants";
 
 export const setAllTeamsArrayContext = function (allTeamsArr: Team[]) {
   setContext(ALL_TEAMS_ARRAY_CONTEXT_KEY, allTeamsArr);
@@ -10,4 +10,13 @@ export const setAllTeamsArrayContext = function (allTeamsArr: Team[]) {
 export const getAllTeamsArrayContext = function () {
   const allTeamsArray = getContext(ALL_TEAMS_ARRAY_CONTEXT_KEY) as Team[];
   return allTeamsArray;
+};
+
+export const setAllPlayersArrayContext = function (allPlayersArr: Player[]) {
+  setContext(ALL_PLAYERS_ARRAY_CONTEXT_KEY, allPlayersArr);
+};
+
+export const getAllPlayersArrayContext = function () {
+  const allPlayersArray = getContext(ALL_PLAYERS_ARRAY_CONTEXT_KEY) as Player[];
+  return allPlayersArray;
 };
