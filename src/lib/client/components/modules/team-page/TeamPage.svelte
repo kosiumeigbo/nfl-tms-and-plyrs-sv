cba216ccaebbdc07ac1116f91663850614aa86d8
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-07 19:56:22 +0100

    chore: create TeamPlayersSection component

--- src/lib/client/components/modules/team-page/TeamPage.svelte
+++ src/lib/client/components/modules/team-page/TeamPage.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import type { Team, Player } from "$lib/types";
   import Header from "@client/components/modules/team-page/Header.svelte";
+  import TeamPlayersSection from "@client/components/modules/team-page/TeamPlayersSection.svelte";
 
   let { team, teamPlayers }: { team: Team; teamPlayers: Player[] } = $props();
-  console.log(teamPlayers);
 </script>
 
 <Header {team} />
+<TeamPlayersSection {teamPlayers} />
---
+++ src/lib/client/components/modules/team-page/TeamPlayersSection.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import type { Player } from "$lib/types";
+
+  let { teamPlayers }: { teamPlayers: Player[] } = $props();
+  console.log(teamPlayers);
+</script>
