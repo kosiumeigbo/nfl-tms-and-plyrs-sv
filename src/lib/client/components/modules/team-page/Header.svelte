985231358e070642772a3da6bd527ecd90c36c41
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-07 22:29:02 +0100

    style: team page Header.svelte

--- src/lib/client/components/modules/team-page/Header.svelte
+++ src/lib/client/components/modules/team-page/Header.svelte
@@ -15,7 +15,7 @@
   const secondaryColor = getSecondaryColor(team);
 </script>
 
-<header class="relative w-full border-b-2 border-b-black py-10">
+<header class="relative h-72 w-full border-b-2 border-b-black">
   <div class="absolute top-0 left-0 -z-10 grid h-full w-full grid-cols-2">
     <div class="bg-white"></div>
     <div class="grid grid-cols-[1fr_4fr]">
@@ -25,13 +25,19 @@
   </div>
   <div class="h-full w-full">
     <div class="custom-container">
-      <div class="grid w-full grid-cols-2">
+      <div class="grid h-full w-full grid-cols-2">
         <div class="font-display flex flex-col items-start justify-center">
           <p class="text-4xl">{team.FullName}</p>
           <p class="text-xl">{team.Conference} {team.Division}</p>
         </div>
-        <div class="flex items-center justify-center">
-          <img src={team.WikipediaLogoUrl} alt={`Logo for ${team.FullName}`} class="h-4/5" />
+        <div>
+          <div class="flex h-full w-full items-center justify-center">
+            <img
+              src={team.WikipediaLogoUrl}
+              alt={`Logo for ${team.FullName}`}
+              class="h-4/5 w-full max-w-2/5 object-contain"
+            />
+          </div>
         </div>
       </div>
     </div>
