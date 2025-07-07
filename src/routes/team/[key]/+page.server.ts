e680c33aa237bec4bf0daeba2dfcb92aff9cef97
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-07 14:22:37 +0100

    refactor: get teamObj in server load function from parent layout data

--- src/routes/team/[key]/+page.server.ts
+++ src/routes/team/[key]/+page.server.ts
@@ -1,6 +1,13 @@
+import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = function ({ params }) {
+export const load: PageServerLoad = async function ({ params, parent }) {
   const { key } = params;
-  return { teamKey: key };
+  const { allTeams } = await parent();
+  const teamObj = allTeams.find((team) => team.Key === key);
+
+  if (!teamObj) {
+    error(400, "Something is wrong as we can't find a team with this key");
+  }
+  return { teamObj };
 };
--- src/routes/team/[key]/+page.svelte
+++ src/routes/team/[key]/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
-  import { getAllTeamsArrayContext } from "$lib/context";
   import TeamPage from "@client/components/modules/team-page/TeamPage.svelte";
   import { MAIN_TITLE } from "$lib/constants";
 
   let { data }: PageProps = $props();
-  const teamObj = getAllTeamsArrayContext().find((team) => team.Key === data.teamKey);
-  console.log(teamObj);
+  const { teamObj } = data;
 
   const getPageTitle = function () {
     if (!teamObj) {
@@ -20,8 +18,4 @@
   <title>{getPageTitle()}</title>
 </svelte:head>
 
-{#if !teamObj}
-  <p>Something is wrong</p>
-{:else}
-  <TeamPage {teamObj} />
-{/if}
+<TeamPage {teamObj} />
