fc93d9e035335d3bcd9fe92a001cfd220f3e98ce
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-07 21:45:12 +0100

    style: edit Team page Header component

--- src/lib/client/components/modules/team-page/Header.svelte
+++ src/lib/client/components/modules/team-page/Header.svelte
@@ -15,12 +15,25 @@
   const secondaryColor = getSecondaryColor(team);
 </script>
 
-<header class="h-20 w-full">
-  <div class="grid h-full w-full grid-cols-2">
+<header class="relative w-full border-b-2 border-b-black py-10">
+  <div class="absolute top-0 left-0 -z-10 grid h-full w-full grid-cols-2">
     <div class="bg-white"></div>
     <div class="grid grid-cols-[1fr_4fr]">
       <div style:background-color={primaryColor}></div>
       <div style:background-color={secondaryColor}></div>
+    </div>
+  </div>
+  <div class="h-full w-full">
+    <div class="custom-container">
+      <div class="grid w-full grid-cols-2">
+        <div class="font-display flex flex-col items-start justify-center">
+          <p class="text-4xl">{team.FullName}</p>
+          <p class="text-xl">{team.Conference} {team.Division}</p>
+        </div>
+        <div class="flex items-center justify-center">
+          <img src={team.WikipediaLogoUrl} alt={`Logo for ${team.FullName}`} class="h-4/5" />
+        </div>
+      </div>
     </div>
   </div>
 </header>
