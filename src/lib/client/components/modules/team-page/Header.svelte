4f30abfd1d1eee9f8059cea564839f4d1f90e3e2
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-08 16:03:10 +0100

    style: show team word mark on team page

--- src/lib/client/components/modules/team-page/Header.svelte
+++ src/lib/client/components/modules/team-page/Header.svelte
@@ -15,31 +15,38 @@
   const secondaryColor = getSecondaryColor(team);
 </script>
 
-<header class="relative h-72 w-full border-b-2 border-b-black">
-  <div class="absolute top-0 left-0 -z-10 grid h-full w-full grid-cols-2">
-    <div class="bg-white"></div>
-    <div class="grid grid-cols-[1fr_4fr]">
-      <div style:background-color={primaryColor}></div>
-      <div style:background-color={secondaryColor}></div>
+<header class="w-full">
+  <section class="relative h-72 w-full border-b-2 border-b-black">
+    <div class="absolute top-0 left-0 -z-10 grid h-full w-full grid-cols-2">
+      <div class="bg-white"></div>
+      <div class="grid grid-cols-[1fr_4fr]">
+        <div style:background-color={primaryColor}></div>
+        <div style:background-color={secondaryColor}></div>
+      </div>
     </div>
-  </div>
-  <div class="h-full w-full">
-    <div class="custom-container">
-      <div class="grid h-full w-full grid-cols-2">
-        <div class="font-display flex flex-col items-start justify-center">
-          <p class="text-4xl">{team.FullName}</p>
-          <p class="text-xl">{team.Conference} {team.Division}</p>
-        </div>
-        <div>
-          <div class="flex h-full w-full items-center justify-center">
-            <img
-              src={team.WikipediaLogoUrl}
-              alt={`Logo for ${team.FullName}`}
-              class="h-4/5 w-full max-w-2/5 object-contain"
-            />
+    <div class="h-full w-full">
+      <div class="custom-container">
+        <div class="grid h-full w-full grid-cols-2">
+          <div class="font-display flex flex-col items-start justify-center">
+            <p class="text-4xl">{team.FullName}</p>
+            <p class="text-xl">{team.Conference} {team.Division}</p>
+          </div>
+          <div>
+            <div class="flex h-full w-full items-center justify-center">
+              <img
+                src={team.WikipediaLogoUrl}
+                alt={`Logo for ${team.FullName}`}
+                class="h-4/5 w-full max-w-2/5 object-contain"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
+  <section class="mt-10 w-full">
+    <div class="custom-container flex items-center justify-center">
+      <img src={team.WikipediaWordMarkUrl} alt={`Word mark for ${team.FullName}`} class="w-full max-w-xl" />
+    </div>
+  </section>
 </header>
