cd055d81e37984ad26b8fa17a3d90b91e69edef0
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-08 22:06:29 +0100

    feat: integrate WeatherAPI

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,5 +1,5 @@
-import type { Team, Player } from "$lib/types";
-import { SPORTSDATA_API_KEY } from "$env/static/private";
+import type { Team, Player, WeatherAPIBadResponse, WeatherAPIGoodResponse } from "$lib/types";
+import { SPORTSDATA_API_KEY, WEATHER_API_KEY } from "$env/static/private";
 
 export const getAllTeamsFromSportsdataIOAPI = async function () {
   const allTeamsAPIUrl = `https://api.sportsdata.io/v3/nfl/scores/json/Teams?key=${SPORTSDATA_API_KEY}`;
@@ -19,4 +19,15 @@ export const getAllAvailablePlayersFromSportsdataIOAPI = async function () {
   }
   const allPlayers = (await res.json()) as Player[];
   return allPlayers;
+};
+
+export const getStadiumWeatherFromWeatherAPI = async function ({ lat, long }: { lat: number; long: number }) {
+  const stadiumWeatherAPIURL = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${lat.toString()},${long.toString()}`;
+  const res = await fetch(stadiumWeatherAPIURL);
+  if (!res.ok) {
+    const errorObj = (await res.json()) as WeatherAPIBadResponse;
+    throw new Error(errorObj.error.message);
+  }
+  const weatherDetails = (await res.json()) as WeatherAPIGoodResponse;
+  return weatherDetails;
 };
