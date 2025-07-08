77b63d58468a2b30e948628ab396c699e585db21
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-08 16:17:15 +0100

    chore: create new TeamInfoSection component

---
+++ src/lib/client/components/modules/team-page/TeamInfoSection.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { Team } from "$lib/types";
+
+  let { team }: { team: Team } = $props();
+</script>
+
+{#snippet coachingStaffAndPlaySchemes()}
+  <div>{JSON.stringify(team)}</div>
+{/snippet}
+
+{@render coachingStaffAndPlaySchemes()}
--- src/lib/client/components/modules/team-page/TeamPage.svelte
+++ src/lib/client/components/modules/team-page/TeamPage.svelte
@@ -2,10 +2,12 @@
   import type { Team, Player } from "$lib/types";
   import Header from "@client/components/modules/team-page/Header.svelte";
   import TeamPlayersSection from "@client/components/modules/team-page/TeamPlayersSection.svelte";
+  import TeamInfoSection from "@client/components/modules/team-page/TeamInfoSection.svelte";
 
   let { team, teamPlayers }: { team: Team; teamPlayers: Player[] } = $props();
   console.log(team);
 </script>
 
 <Header {team} />
+<TeamInfoSection {team} />
 <TeamPlayersSection {teamPlayers} />
