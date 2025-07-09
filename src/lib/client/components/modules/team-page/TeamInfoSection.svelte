53308d5bd3f855ae9a6357889c14506e2c100ca2
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-09 22:24:06 +0100

    style: update TeamInfoSection.svelte component

--- src/lib/client/components/modules/team-page/TeamInfoSection.svelte
+++ src/lib/client/components/modules/team-page/TeamInfoSection.svelte
@@ -5,7 +5,7 @@
   type StadiumWeatherPromiseState = Promise<WeatherAPIGoodResponse>;
 
   let { team }: { team: Team } = $props();
-  const { StadiumDetails, ...rest } = team;
+  const { StadiumDetails } = team;
   let stadiumWeatherPromise: StadiumWeatherPromiseState = $derived(
     getStadiumWeatherData({ lat: StadiumDetails.GeoLat, long: StadiumDetails.GeoLong }),
   );
@@ -33,23 +33,67 @@
 
 {#snippet coachingStaffAndPlaySchemesSection()}
   <div class="w-full">
-    {JSON.stringify(rest)}
     <h1 class="text-center text-5xl">Coaching Staff and Play Schemes</h1>
     <div class="grid grid-cols-3">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <p class="uppercase">Head Coach</p>
+        <p>{team.HeadCoach}</p>
+      </div>
+      <div>
+        <p class="uppercase">Offensive Coordinator</p>
+        <p>{team.OffensiveCoordinator}</p>
+      </div>
+      <div>
+        <p class="uppercase">Defensive Coordinator</p>
+        <p>{team.DefensiveCoordinator}</p>
+      </div>
+      <div>
+        <p class="uppercase">Special Teams Coach</p>
+        <p>{team.SpecialTeamsCoach}</p>
+      </div>
+      <div>
+        <p class="uppercase">Defensive Scheme</p>
+        <p>{team.DefensiveScheme}</p>
+      </div>
+      <div>
+        <p class="uppercase">Offensive Scheme</p>
+        <p>{team.OffensiveScheme}</p>
+      </div>
     </div>
   </div>
 {/snippet}
 
 {#snippet stadiumDetailsSection()}
-  <div class="w-full"><img src={`/stadiums/${team.Key}.jpg`} alt={`Stadium photo for ${team.FullName}`} /></div>
-  {@render getCurrentWeatherDetailsText()}
-  {@render getRegionStateText()}
+  <div class="w-full">
+    <h1 class="text-center text-5xl">Stadium Details</h1>
+    <div class="w-full"><img src={`/stadiums/${team.Key}.jpg`} alt={`Stadium photo for ${team.FullName}`} /></div>
+    <div class="grid grid-cols-3">
+      <div>
+        <p class="uppercase">Name</p>
+        <p>{StadiumDetails.Name}</p>
+      </div>
+      <div>
+        <p class="uppercase">Capacity</p>
+        <p>{StadiumDetails.Capacity}</p>
+      </div>
+      <div>
+        <p class="uppercase">City</p>
+        <p>{StadiumDetails.City}</p>
+      </div>
+      <div>
+        <p class="uppercase">State</p>
+        <p>{@render getRegionStateText()}</p>
+      </div>
+      <div>
+        <p class="uppercase">Playing Surface</p>
+        <p>{StadiumDetails.PlayingSurface}</p>
+      </div>
+      <div>
+        <p class="uppercase">Current Weather Details</p>
+        <p>{@render getCurrentWeatherDetailsText()}</p>
+      </div>
+    </div>
+  </div>
 {/snippet}
 
 <section class="w-full">
