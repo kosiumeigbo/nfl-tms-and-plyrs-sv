d4b511711c1a47808d5d60bd265b0bff8f13b4b3
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-10 18:17:24 +0100

    chore: rename component file name

--- src/lib/client/components/modules/team-page/TeamPage.svelte
+++ src/lib/client/components/modules/team-page/TeamPage.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { Team, Player } from "$lib/types";
   import Header from "@client/components/modules/team-page/Header.svelte";
-  import TeamPlayersSection from "@client/components/modules/team-page/TeamPlayersSection.svelte";
+  import TeamRosterSection from "@client/components/modules/team-page/TeamRosterSection.svelte";
   import TeamInfoSection from "@client/components/modules/team-page/TeamInfoSection.svelte";
 
   let { team, teamPlayers }: { team: Team; teamPlayers: Player[] } = $props();
@@ -10,4 +10,4 @@
 
 <Header {team} />
 <TeamInfoSection {team} />
-<TeamPlayersSection {teamPlayers} />
+<TeamRosterSection {teamPlayers} />
