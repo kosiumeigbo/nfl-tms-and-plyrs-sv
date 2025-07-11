074e708778e2b310a8dc0e906c01ef3ef92ff9c0
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-11 23:40:38 +0100

    style: add responsive design for TeamRosterSection component

--- src/lib/client/components/modules/team-page/TeamRosterSection.svelte
+++ src/lib/client/components/modules/team-page/TeamRosterSection.svelte
@@ -21,17 +21,17 @@
 
 <section class="font-display mt-10">
   <div class="custom-container">
-    <h1 class="mb-5 text-center text-5xl">Roster</h1>
+    <h1 class="mb-6 text-center text-3xl md:text-4xl lg:text-5xl">Roster</h1>
     {#if teamPlayers.length === 0}
       <p>No players on this team</p>
     {:else}
-      <div class="grid grid-cols-4 gap-y-3">
+      <div class="xs:grid-cols-2 grid grid-cols-1 gap-y-3 text-center md:grid-cols-3 lg:grid-cols-4">
         {#each teamPlayers as player (player.PlayerID)}
           <div>
             <a
               href={`/player/${player.PlayerID}`}
               class={{
-                "text-xl text-stone-400": true,
+                "text-base text-stone-400 md:text-lg lg:text-xl": true,
                 "hover:text-afc": getTextColor(player) === "AFC",
                 "hover:text-nfc": getTextColor(player) === "NFC",
               }}
