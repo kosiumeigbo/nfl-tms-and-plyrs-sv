f9be35ceedd6d83b112b9a7833951b824510f02f
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-11 22:38:25 +0100

    style: update TeamRosterSection component

--- src/lib/client/components/modules/team-page/TeamInfoSection.svelte
+++ src/lib/client/components/modules/team-page/TeamInfoSection.svelte
@@ -37,27 +37,27 @@
     <div class="grid grid-cols-3 gap-y-5">
       <div>
         <p class="text-2xl uppercase">Head Coach</p>
-        <p class="text-2xl">{team.HeadCoach}</p>
+        <p class="text-xl">{team.HeadCoach}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">Offensive Coordinator</p>
-        <p class="text-2xl">{team.OffensiveCoordinator}</p>
+        <p class="text-xl">{team.OffensiveCoordinator}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">Defensive Coordinator</p>
-        <p class="text-2xl">{team.DefensiveCoordinator}</p>
+        <p class="text-xl">{team.DefensiveCoordinator}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">Special Teams Coach</p>
-        <p class="text-2xl">{team.SpecialTeamsCoach}</p>
+        <p class="text-xl">{team.SpecialTeamsCoach}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">Defensive Scheme</p>
-        <p class="text-2xl">{team.DefensiveScheme}</p>
+        <p class="text-xl">{team.DefensiveScheme}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">Offensive Scheme</p>
-        <p class="text-2xl">{team.OffensiveScheme}</p>
+        <p class="text-xl">{team.OffensiveScheme}</p>
       </div>
     </div>
   </div>
@@ -66,31 +66,33 @@
 {#snippet stadiumDetailsSection()}
   <div class="mb-20 w-full">
     <h1 class="mb-5 text-center text-5xl">Stadium Details</h1>
-    <div class="mb-5 w-full"><img src={`/stadiums/${team.Key}.jpg`} alt={`Stadium photo for ${team.FullName}`} /></div>
+    <div class="mb-5 flex w-full justify-center">
+      <img src={`/stadiums/${team.Key}.jpg`} alt={`Stadium photo for ${team.FullName}`} />
+    </div>
     <div class="grid grid-cols-3 gap-y-5">
       <div>
         <p class="text-2xl uppercase">Name</p>
-        <p class="text-2xl">{StadiumDetails.Name}</p>
+        <p class="text-xl">{StadiumDetails.Name}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">Capacity</p>
-        <p class="text-2xl">{StadiumDetails.Capacity}</p>
+        <p class="text-xl">{StadiumDetails.Capacity}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">City</p>
-        <p class="text-2xl">{StadiumDetails.City}</p>
+        <p class="text-xl">{StadiumDetails.City}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">State</p>
-        <p class="text-2xl">{@render getRegionStateText()}</p>
+        <p class="text-xl">{@render getRegionStateText()}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">Playing Surface</p>
-        <p class="text-2xl">{StadiumDetails.PlayingSurface}</p>
+        <p class="text-xl">{StadiumDetails.PlayingSurface}</p>
       </div>
       <div>
         <p class="text-2xl uppercase">Current Weather Details</p>
-        <p class="text-2xl">{@render getCurrentWeatherDetailsText()}</p>
+        <p class="text-xl">{@render getCurrentWeatherDetailsText()}</p>
       </div>
     </div>
   </div>
