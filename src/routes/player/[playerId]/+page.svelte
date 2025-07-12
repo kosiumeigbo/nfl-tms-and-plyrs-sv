f608f9b12083aceb6a443f56cead8744b7a4f271
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-07-12 15:30:16 +0100

    chore: mount Header on player +page.svelte

--- src/routes/player/[playerId]/+page.svelte
+++ src/routes/player/[playerId]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
   import { MAIN_TITLE } from "$lib/constants";
+  import Header from "$lib/client/components/modules/player-page/Header.svelte";
 
   let { data }: PageProps = $props();
   const { player, team } = data;
@@ -11,3 +12,5 @@
 <svelte:head>
   <title>{`${MAIN_TITLE} | ${player.Name}`}</title>
 </svelte:head>
+
+<Header {player} {team} />
